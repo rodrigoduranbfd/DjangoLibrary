690f8d6c3b8009c1122b73f67c68d2b0434ad250
rodrigoduranbfd <1875139@etfbsb.edu.br>
2025-10-26 21:57:17 -0300

    FIX - Corrigindo o settings.py

--- library/settings.py
+++ library/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'library.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'library/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
