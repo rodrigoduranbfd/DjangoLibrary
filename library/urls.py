5cdc4b2fd30a1fc8ab481cd61a6b17b35988f876
rodrigoduranbfd <1875139@etfbsb.edu.br>
2025-10-26 23:09:49 -0300

    alterando as urls e o template de login

--- library/urls.py
+++ library/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from catalog import views as catalog_views
 from borrowing import views as borrowing_views
 from django.contrib.auth import views as auth_views
+from borrowing.views import register_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -20,4 +21,5 @@ urlpatterns = [
     # Auth
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', register_view, name='register')  # ← Adicionado
 ]
